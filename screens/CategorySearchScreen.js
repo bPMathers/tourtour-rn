30d14dfb5a09781deadbeb4f1deb4ae7178cdf10
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-05-30 17:27:29 -0400

    -send basic data from db to CategorySearchScreen

--- screens/CategorySearchScreen.js
+++ screens/CategorySearchScreen.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
 import { TourTourColors } from '../constants/Colors'
 
+const getPlaces = gql`
+  query {
+    places{
+      id 
+      name
+    }
+  }
+`
 
 const CategorySearchScreen = (props) => {
-    const location = "Montréal"
+    const { loading, error, data } = useQuery(getPlaces);
+
+    const renderGridItem = (itemData) => {
+        return (
+            <View style={styles.listItem}>
+                <Text>{itemData.item.name}</Text>
+            </View>
+        );
+    };
+
+    if (loading) return <View style={styles.container}><Text>Loading...</Text></View>;
+    if (error) return <View style={styles.container}><Text>Error...</Text></View>;
 
     return (
         <View style={styles.container}>
@@ -15,6 +36,7 @@ const CategorySearchScreen = (props) => {
             <View>
                 <Button title="Changer de location" color={TourTourColors.primary}></Button>
             </View>
+            <FlatList data={data.places} renderItem={renderGridItem} />
         </View>
     );
 }
@@ -22,7 +44,15 @@ const CategorySearchScreen = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    }
+    },
+    listItem: {
+        flex: 1,
+        margin: 2,
+        height: 150,
+        borderRadius: 5,
+        elevation: 5,
+        overflow: Platform.OS === "android" ? "hidden" : "hidden",
+    },
 })
 
 export default CategorySearchScreen;
