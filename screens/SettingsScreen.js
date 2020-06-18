50d04266b700267535abf524c5731ec35e199bc6
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-06-18 16:48:25 -0400

    -navigate to home on logout

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -3,14 +3,14 @@ import { View, Text, StyleSheet, Button, AsyncStorage } from 'react-native';
 import { useApolloClient } from '@apollo/react-hooks'
 
 
-const SettingsScreen = () => {
+const SettingsScreen = (props) => {
   const client = useApolloClient()
 
   return (
     <View style={styles.container}>
       <Button title="Temp Logout" color="red" onPress={() => {
         AsyncStorage.removeItem('userToken')
-        // props.navigation.navigate('Auth')
+        props.navigation.navigate('Home')
         client.resetStore()
       }} />
     </View>
